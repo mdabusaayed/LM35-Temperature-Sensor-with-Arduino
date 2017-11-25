5bb666d2f0ef8e1ebe8d2d9931061d2c44dc8007
Mohammad Abu Sayed <mdabusaayed@gmail.com>
2017-11-25 20:07:04 +0600

    Create LM35_Ard.ino

---
+++ LM35_Ard.ino
@@ -0,0 +1,19 @@
+float tempC;
+int reading;
+int tempPin = 0;
+
+void setup()
+{
+  analogReference(INTERNAL);
+Serial.begin(9600);
+}
+
+void loop()
+{
+reading = analogRead(tempPin);
+tempC = reading / 9.31;
+Serial.print("temperature: ");
+Serial.print(tempC);
+Serial.println(" ");
+delay(1000);
+}
